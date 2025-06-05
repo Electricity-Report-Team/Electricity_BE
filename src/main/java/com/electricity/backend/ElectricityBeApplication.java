73971e606bdbed749cf9b45dc4c884fec8edde72
dodohan1224 <kdymmm1224@naver.com>
2025-06-05 22:00:30 +0900

    Initial Spring Boot project setup

---
+++ src/main/java/com/electricity/backend/ElectricityBeApplication.java
@@ -0,0 +1,13 @@
+package com.electricity.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityBeApplication.class, args);
+	}
+
+}
